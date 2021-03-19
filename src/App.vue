6d147af7d816ae9e92d604e40026148b38ce93f3
xutengteng007 <xuteng433>
2021-03-19 16:14:08 +0800

    c code

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <Index></Index>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Index from './components/dashboard/Index.vue';
+
+@Component({
+  components: {
+    Index,
+  },
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/dashboard/Index.vue
@@ -0,0 +1,21 @@
+<template>
+  <WatermelonDrawer/>
+</template>
+
+<script lang="ts">
+import {Vue} from "vue-property-decorator";
+import Component from "vue-class-component";
+import WatermelonDrawer from "@/components/dashboard/component/core/WatermelonDrawer.vue";
+
+@Component({
+  components:{
+    WatermelonDrawer
+  }
+})
+export default class Index extends Vue {
+}
+</script>
+
+<style scoped>
+
+</style>
