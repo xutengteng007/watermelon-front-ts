e2c5b5f909837056cb8b9e036920565c38f3369c
xutengteng007 <xuteng433>
2021-03-20 11:33:20 +0800

    c code

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,9 +1,22 @@
 import Vue from 'vue'
 import Vuetify from 'vuetify'
 import 'vuetify/dist/vuetify.min.css'
+import '@/sass/overrides.sass'
 
 Vue.use(Vuetify)
-
-const opts = {}
+const theme = {
+    primary: '#4CAF50',
+    secondary: '#9C27b0',
+    accent: '#9C27b0',
+    info: '#00CAE3',
+}
+const opts = {
+    theme: {
+        themes: {
+            dark: theme,
+            light: theme,
+        },
+    },
+}
 
 export default new Vuetify(opts)
